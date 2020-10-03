0b384f21acee139189193f4250d308053ed3521a
Aze <azrielle101@gmail.com>
2020-10-03 19:39:34 +0800

    Created handleOutput and handleForm that will determine which component appears. These 2 functions are handed down as props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react';
 import Form from './components/form/Form';
+import Output from './components/Output';
+
 import './css/App.css';
 
-function App() {
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      form: true,
+      output: false
+    }
+
+    this.handleOutput = this.handleOutput.bind(this);
+    this.handleForm = this.handleForm.bind(this);
+  }
+
+  handleOutput() {
+    this.setState({
+      form: false,
+      output: true
+    })
+  }
+
+  handleForm() {
+    this.setState({
+      form: true,
+      output: false
+    })
+  }
+
+render() {
   return (
     <div>
-      <Form />
+      {this.state.form && <Form moveToOutput = {this.handleOutput}/>}
+      {this.state.output && <Output moveToForm = {this.handleForm} />}
+
     </div>
-  );
+  )
+}
 }
 
 export default App;
