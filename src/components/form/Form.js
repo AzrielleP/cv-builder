baa94141080691a14fad3005d83b21a5dbca119e
Aze <azrielle101@gmail.com>
2020-10-03 19:40:13 +0800

    Initialize the states and handleInputChange that will be passed down as props. Set up the handleSubmit and componentDidMount() for localStorage and for switching to the Output.js component.

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -5,11 +5,120 @@ import './css/form.css';
 import GeneralInfo  from './form-child-components/General-Info';
 
 class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+                name: '',
+                title: '',
+                careerSummary: '',
+                contact: {
+                    phone: '',
+                    email: '',
+                    address: '',
+                    website: '',
+                    linkedIn: '',
+                    gitHub: '',
+                },
+            
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    // handleInputChange(event, category, subCategory) {
+        
+    //     const { name, value, className } = event.target;
+    //     className === 'subCategory' ?
+    //     this.setState({
+    //       ...this.state,
+    //       [category]: {
+    //         [subCategory]: {
+    //             ...this.state[category][subCategory],
+    //             [name]: value,
+    //         },
+    //       },
+    //     })
+    //     : 
+    //     this.setState({
+    //         ...this.state,
+    //         [category]: {
+    //             [name]: value,
+    //            [subCategory]: {
+    //                ...this.state[category][subCategory]
+    //            }
+    //         },
+            
+    //     })
+    // }
+
+    componentDidMount() {
+        const data = JSON.parse(localStorage.getItem('data'));
+        if (localStorage.getItem('data')) {
+          this.setState({
+            name: data.name,
+            title: data.title,
+            careerSummary: data.careerSummary,
+            contact: {
+              phone: data.contact.phone,
+              email: data.contact.email,
+              address: data.contact.address,
+              website: data.contact.website,
+              linkedIn: data.contact.linkedIn,
+              gitHub: data.contact.gitHub,
+            },
+          })
+        }
+        else {
+          this.setState({
+            name: '',
+            title: '',
+            careerSummary: '',
+            contact: {
+              phone: '',
+              email: '',
+              address: '',
+              website: '',
+              linkedIn: '',
+              gitHub: '',
+            }
+          })
+        }
+      }
+
+    handleInputChange(event) {
+        
+        const { name, value, className } = event.target;
+        className === 'subCategory' ?
+        this.setState({
+          ...this.state,
+          contact: {
+            ...this.state.contact,
+            [name]: value,
+            },
+        })
+        : 
+        this.setState({
+            [name]:value,
+            contact: {
+                ...this.state.contact
+            }
+            
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.moveToOutput();
+        localStorage.setItem('data', JSON.stringify(this.state));
+        
+    }
 
     render() {
         return(
-            <form>
-                <GeneralInfo />
+            <form onSubmit = {this.handleSubmit}>
+                <GeneralInfo handleInputChange = {this.handleInputChange} generalInfo = {this.state} />
                 <button className = "generateButton">Generate</button>
             </form>
         )
