17b87d4cc361cbee4ccb7cb80603acbfa555d6a2
Aze <azrielle101@gmail.com>
2020-10-07 00:57:12 +0800

    Added an SVG beside <h2>

--- src/components/form/form-child-components/GeneralInfoForm.js
+++ src/components/form/form-child-components/GeneralInfoForm.js
@@ -6,7 +6,27 @@ export default function GeneralInfo(props) {
 
   return (
     <div className="generalInfo">
-      <h2>General Info</h2>
+      <h2> 
+      <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d)">
+        <rect width="23" height="23" fill="#1D3557"/>
+        </g>
+        <defs>
+        <filter id="filter0_d" x="0" y="0" width="27" height="27" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+        <feOffset dx="4" dy="4"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+        </filter>
+        </defs>
+      </svg>
+      <span>
+        General Info
+      </span>
+      
+      </h2>
       <div className="divider">
         <div>
           <label htmlFor="name">
